5344b780399d705c32032b26a9c87eea4fb34b87
Caden Hong <caden.p.hong@gmail.com>
2022-09-12 12:55:17 -0400

    Setting up project

---
+++ buildscript4.py
@@ -0,0 +1,27 @@
+#################################################################################
+#
+# Name: Caden Hong
+#
+# Date: September 12, 2022
+#
+# Description: This script :
+#	- 
+#	- 
+#	- 
+#	- 
+#
+# Potential Issues:
+#	- 
+#	- 
+#	- 
+#
+##################################################################################
+
+import os
+import requests
+
+# os.system("pip3 install requests")
+
+# api_url = "https://jsonplaceholder.typicode.com/todos/1"
+# response = requests.get(api_url)
+# response.json()
