8ccf2f5fb90c50a024973299b63e8329579ec3bc
Caden Hong <caden.p.hong@gmail.com>
2022-09-15 13:08:18 -0400

    informational prompt edit

--- setup.sh
+++ setup.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+echo "============================================================================================"
+echo "Checking environment for necessary package to run this script..."
+sleep 2
+
 package=$(pip3 list | grep "requests")
 
 if [ -z "$package" ] ; then
-  echo $package "does not exist, installing now..."
+  echo "requests package does not exist, installing now..."
   sudo apt update
   sudo apt upgrade -y
   pip3 install requests
-  echo "Installation completed"
 else
   echo $package "exists"
 fi
