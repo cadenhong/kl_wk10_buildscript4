639d698f223e6c4b07e4741874f9f209f4f40ea6
Caden Hong <caden.p.hong@gmail.com>
2022-09-14 22:23:43 -0400

    editing code to store select info in list of lists

--- buildscript4.py
+++ buildscript4.py
@@ -20,6 +20,7 @@
 import subprocess
 import requests
 import json
+import csv
 
 #subprocess.call(['./setup.sh'])
 
@@ -27,9 +28,8 @@ import json
 #api_url = f'https://api.github.com/users/{user}/repos'
 
 # username = 'cadenhong'
-# user_api_url = f'https://api.github.com/users/cadenhong'
+
 # repo_api_url = f'https://api.github.com/users/cadenhong/repos'
-# user_response = requests.get(user_api_url, auth=(username,token)).json()
 # repo_response = requests.get(repo_api_url, auth=(username,token)).json()
 
 # print(user_response['name'])
@@ -49,19 +49,99 @@ import json
 
 # ------------------------ working with json files -------------------------------
 
-info = {'name': [], 'description': [], 'language': [], 'html_url': [], 'created_at': [], 'updated_at': []}
-headers = list(info.keys())
-print(headers)
-
-with open('user.json', 'r') as user:
-    u = json.load(user)
+headers = ['name', 'description', 'language', 'html_url', 'created_at', 'updated_at']
 
 with open('repo.json', 'r') as repo:
     r = json.load(repo)
 
-print(r[0]['owner']['login'])
+dct = {}
+lst = []
 
-for repo in range(len(r)):
-    for header in range(len(headers)):
-        print(r[repo][headers[header]], end=", ")
-    print()
+n = [ {'name': 'a', 'description': 'a', 'language': '', 'html_url': '', 'created_at': '', 'updated_at': ''},\
+      {'name': 'b', 'description': 'b', 'language': '', 'html_url': '', 'created_at': '', 'updated_at': ''},\
+      {'name': 'c', 'description': 'c', 'language': '', 'html_url': '', 'created_at': '', 'updated_at': ''},\
+      {'name': 'd', 'description': 'd', 'language': '', 'html_url': '', 'created_at': '', 'updated_at': ''} ]
+
+print(n[0]['name']) # a
+
+# list of field values (i.e [name, desc, date, etc, etc, etc])
+# dict(zip(header,list)) to create a dictionary, then append it to a list
+# repeat for all the repos (12 total)
+
+b = []
+for repo in r:
+    a = []
+    for header in headers:
+#        print(repo[header])
+        #a = []
+        a.append(repo[header])
+    b.append(a)
+print(b)
+#    dictionary = dict(zip(headers,a))
+
+
+# print(len(dictionary))
+
+# # prints by header info 
+# for header in headers:
+#     for repository in r:
+#         print(repository[header])
+
+# for num in range(len(r)):
+#     for header in headers:
+#         dct[header] = r[num][header]
+# print(len(r))
+
+# with open('testing.txt', 'w') as t:
+#     # prints all header by repo
+#     for repos in r:
+#         for h in headers:
+#             t.write(repos[h])
+        
+
+# for num in range(len(r)):
+#     for header in headers:
+#         lst.append({header})
+#         #dct[header] = r[num][header]
+# print(len(lst))   
+# #    print(dct)
+# #print(len(dct))
+
+# for num in range(len(r)):
+#     for header in headers:
+#         lst.append({header:r[num][header]})
+#         # dct[header] = r[header]
+#         # lst.append(dct)
+
+# print(lst[0])
+
+# print(len(lst))
+
+# for num in range(len(r)):
+#     for header in headers:
+#         index = 0
+#         info[header].append(r[num][header])
+#         index += 1
+
+# for num in range(len(r)):
+#     for header in headers:
+#         lst.append({header:r[header]})
+#         print(lst)
+
+# with open('{}.csv'.format(r[0]['owner']['login']), 'w') as f:
+#     writer = csv.DictWriter(f, fieldnames=headers)
+#     writer.writeheader()
+#     writer.writerows(info)
+
+# new_json = []
+
+# for i in range(len(r)):
+#     for field in fields:
+#         dict_pair = {}
+#         dict_pair[field] = r[i][field]
+#         new_json[i].append(dict_pair)
+#         print(new_json)
+#        new_json[i] = field
+ #       new_json[field].append(r[i][field])
+
+#print(new_json)
